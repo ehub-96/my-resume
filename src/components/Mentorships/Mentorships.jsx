9f6511e908306aff81b4e51328579495c94a7fe0
Erik <ehub96.dev@gmail.com>
2022-12-03 01:51:59 -0700

    64 4646468486956

--- src/components/Mentorships/Mentorships.jsx
+++ src/components/Mentorships/Mentorships.jsx
@@ -10,7 +10,7 @@ export class Mentorships extends Card {
             <h1 className='s-title'>Mentorships</h1>
             <div className="row">
             <div class="card" style="width: 18rem;">
-                <img src="cheems" class="card-img-top" alt="..."/>
+                <img src="..." class="card-img-top" alt="..."/>
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
