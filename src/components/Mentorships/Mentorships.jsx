df7e33cc4f968944560abd8aa75e6dd247381f6b
Erik <ehub96.dev@gmail.com>
2022-12-03 01:46:11 -0700

    boi

--- src/components/Mentorships/Mentorships.jsx
+++ src/components/Mentorships/Mentorships.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Ehub from '../../assets/ehub.png'
+import cheems from '../../assets/cheems.jpg'
 
 export class Mentorships extends Card {
     render() {
       return (
-                <div className='Mentorships'>
-                <h1 className='s-title'>Mentorships</h1>
-                <div className="row">
-                <div class="row row-cols-1 row-cols-md-2 g-4">
-                <div class="card border-danger mb-3" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="..."/>
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                    </div>
+        <div className='Mentorships'>
+            <h1 className='s-title'>Mentorships</h1>
+            <div className="row">
+            <div class="card" style="width: 18rem;">
+                <img src="cheems" class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
-            </div>
+                </div>
+          </div>
         </div>
         );
     }
