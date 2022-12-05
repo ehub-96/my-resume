921a457c531f534977efaa6ce7485d5848fd4cf6
Erik <ehub96.dev@gmail.com>
2022-12-04 23:20:24 -0700

    testing icon in certificates

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -13,7 +13,7 @@ export class Certifications extends Component {
               <div class="accordion-item">
                 <h2 class="accordion-header" id="panelsStayOpen-headingOne">
                   <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                    Accordion Item #1
+                    Accordion Item #1 <i class="bi bi-mortarboard-fill"></i>
                   </button>
                 </h2>
                 <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
