130447228df93e878c9503324a6af7f3877a8ee3
Erik <ehub96.dev@gmail.com>
2022-12-04 21:18:51 -0700

    fomx

--- src/components/Mentorships/Mentorships.jsx
+++ src/components/Mentorships/Mentorships.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import cheems from '../../assets/card-image/cheems.jpg'
+
 
 
 export class Mentorships extends Component {
