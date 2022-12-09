7dc062f9f6911385bdbc3982b3a49288e54b0c87
Erik <ehub96.dev@gmail.com>
2022-12-08 23:51:28 -0700

    DONE

--- src/components/Mentorships/Mentorships.jsx
+++ src/components/Mentorships/Mentorships.jsx
@@ -36,32 +36,32 @@ export class Mentorships extends Component {
               </div>
               <div class="col">
                 <div class="card border-danger mb-3">
-                  <img src="https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Fnewsfeed%2F001%2F658%2F936%2F411.jpg" class="card-img-top2" alt="..."/>
+                  <img src="https://writedirection.com/website/wp-content/uploads/2016/09/blank-profile-picture-973460_960_720.png" class="card-img-top2" alt="..."/>
                   <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    <h5 class="card-title">Erwin Arndt</h5>
+                    <p class="card-text">Office IT Administrator - Operations Technician at Westpay AB</p>
                   </div>
                   <div class="card-footer">
                   <div className="col-sm text-center">
-                    <a href="https://github.com/ehub-96" target="_blank" rel="noopener noreferrer"><Github className='card-icon2' /></a>
-                    <a href="https://www.linkedin.com/in/erik-hub-0346001b6/" target="_blank" rel="noopener noreferrer"><Linkedin className='card-icon2' /></a>
-                    <a href="https://twitter.com/ehub96?t=hQ_1F1a5bBjxX6k1RJCqQg&s=08" target="_blank" rel="noopener noreferrer"><Twitter className='card-icon2' /></a>
+                    
+                    <a href="https://www.linkedin.com/in/erwinjohannarndt/" target="_blank" rel="noopener noreferrer"><Linkedin className='card-icon2' /></a>
+                    
                   </div>
                   </div>
                 </div>
               </div>
               <div class="col">
                 <div class="card border-danger mb-3">
-                  <img src="https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Fnewsfeed%2F001%2F658%2F936%2F411.jpg" class="card-img-top2" alt="..."/>
+                  <img src="https://writedirection.com/website/wp-content/uploads/2016/09/blank-profile-picture-973460_960_720.png" class="card-img-top2" alt="..."/>
                   <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    <h5 class="card-title">Lucas Maldonado</h5>
+                    <p class="card-text"></p>
                   </div>
                   <div class="card-footer">
                   <div className="col-sm text-center">
-                    <a href="https://github.com/ehub-96" target="_blank" rel="noopener noreferrer"><Github className='card-icon2' /></a>
+                    {/* <a href="https://github.com/ehub-96" target="_blank" rel="noopener noreferrer"><Github className='card-icon2' /></a>
                     <a href="https://www.linkedin.com/in/erik-hub-0346001b6/" target="_blank" rel="noopener noreferrer"><Linkedin className='card-icon2' /></a>
-                    <a href="https://twitter.com/ehub96?t=hQ_1F1a5bBjxX6k1RJCqQg&s=08" target="_blank" rel="noopener noreferrer"><Twitter className='card-icon2' /></a>
+                    <a href="https://twitter.com/ehub96?t=hQ_1F1a5bBjxX6k1RJCqQg&s=08" target="_blank" rel="noopener noreferrer"><Twitter className='card-icon2' /></a> */}
                   </div>
                   </div>
                 </div>
