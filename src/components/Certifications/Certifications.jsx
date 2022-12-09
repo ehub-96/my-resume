ce329729fce32867b725c4bbcc833eb2dec84a02
Erik <ehub96.dev@gmail.com>
2022-12-08 21:26:38 -0700

    h5

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -12,15 +12,15 @@ export class Certifications extends Component {
             <div className="row">
             <div class="accordion" id="accordionPanelsStayOpenExample">
               <div class="accordion-item">
-                <h1 class="accordion-header" id="panelsStayOpen-headingOne">
+                <h5 class="accordion-header" id="panelsStayOpen-headingOne">
                   <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne">
                     Data Analysis & Visualization <div class="btn-container text-center"><a href="../../../DA.pdf" target="_blank" rel="noopener noreferrer"><MortarboardFill className='card-icon2' /></a></div>
                   </button>
-                </h1>
+                </h5>
                 <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
                   <div class="accordion-body">
                     <strong>2022 Python Data Analysis & Visualization Masterclass</strong> by Colt Steele in Udemy. 201 lectures with a total of 20.5 hours. This course deeply explores Pandas, Matplotlib, Seaborn while working with Jupyter Notebooks, accomplishing:
-                    
+
                     <li>Read and manipulate Datasets</li>
                     <li>DataFrames and Series objects</li>
                     <li>Organize, filter, clean, aggregate, and analyze DataFrames</li>
