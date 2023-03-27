27bcedd7712b49f2afbeb2ca3a04ac2bcec455a3
Erik Huß <ehub96.dev@gmail.com>
2023-03-27 17:29:57 -0600

    new accordion certif

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -1,47 +1,76 @@
 import React, { Component } from "react";
 import { MortarboardFill, Mortarboard} from 'react-bootstrap-icons';
-import { Accordion, Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
+
+
+
 export class Certifications extends Component {
-    render() {
-      return (
-        <div className='Certifications'>
-            <h1 className='s-title'>Certifications</h1>
-            <div className="row">
-            <div class="accordion" id="accordionPanelsStayOpenExample">
-              <div class="accordion-item">
-                <h5 class="accordion-header" id="panelsStayOpen-headingOne">
-                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne">
-                    Data Analysis & Visualization <div class="btn-container text-center"><a href="../../../DA.pdf" target="_blank" rel="noopener noreferrer"><MortarboardFill className='card-icon2' data-bs-toggle="tooltip" data-bs-placement="top" title="See Diploma"/></a></div>
-                  </button>
-                </h5>
-                <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
-                  <div class="accordion-body">
-                    <strong>2022 Python Data Analysis & Visualization Masterclass</strong> by Colt Steele in Udemy. A total of 20.5 hours. This course deeply explores Pandas, Matplotlib, Seaborn while working with Jupyter Notebooks, accomplishing:
-                    <div className="col">
-                    <p className="p-about">
-                    <li>Read and manipulate Datasets</li>
-                    <li>DataFrames and Series objects</li>
-                    <li>Organize, filter, clean, aggregate, and analyze DataFrames</li>
-                    <li>Extract and manipulate date, time, and textual information from data</li>
-                    <li>Hierarchical Indexing</li>
-                    <li>Merge Datasets</li>
-                    <li>Create complex visualizations</li>
-                    </p>
-                    </div>
+  render() {
+    return (
+      <div className='Certifications'>
+          <h1 className='s-title'>Certifications</h1>
+          <div className="row">
+          <div class="accordion" id="accordionPanelsStayOpenExample">
+
+          <div class="accordion-item">
+              <h5 class="accordion-header" id="panelsStayOpen-headingOne">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne">
+                  Data Eingineering<div class="btn-container text-center"><a href="" target="_blank" rel="noopener noreferrer"><Mortarboard className='card-icon2' data-bs-toggle="tooltip" data-bs-placement="top" title="Certification tooltip" /></a></div>
+                </button>
+              </h5>
+              <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
+                <div class="accordion-body">
+                  <strong>2023 Data Eingineering Zoomcamp</strong> by <a href="https://github.com/DataTalksClub/data-engineering-zoomcamp" class="stretched-link">Data Talks Club</a>. An intensive 14-week training program that provided a comprehensive and immersive learning experience with both pre recorded classes and live tutoring with <a href="https://www.linkedin.com/in/agrigorev/" class="stretched-link">Alexey Grigorev</a> and other <a href="https://github.com/DataTalksClub/data-engineering-zoomcamp#instructors" class="stretched-link">instructors</a> focusing on real-world projects and industry-relevant tools and techniques:
+                  <div className="col">
+                  <p className="p-about">
+                  <li>Docker and containerization</li>
+                  <li>Workflow Orchestration</li>
+                  <li>Data Warehousing and Cloud Services</li>
+                  <li>Analyitics Engineering</li>
+                  <li>Batch Processing</li>
+                  <li>Streaming</li>
+                
+                  </p>
+                  </div>
+
+                  </div>
+                </div>
+              </div>
+
+
+            <div class="accordion-item">
+              <h5 class="accordion-header" id="panelsStayOpen-headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                  Data Analysis & Visualization <div class="btn-container text-center"><a href="../../../DA.pdf" target="_blank" rel="noopener noreferrer"><MortarboardFill className='card-icon2' data-bs-toggle="tooltip" data-bs-placement="top" title="Certification tooltip" /></a></div>
+                </button>
+              </h5>
+              <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+                <div class="accordion-body">
+                  <strong>2022 Python Data Analysis & Visualization Masterclass</strong> by Colt Steele in Udemy. A total of 20.5 hours. This course deeply explores Pandas, Matplotlib, Seaborn while working with Jupyter Notebooks, accomplishing:
+                  <div className="col">
+                  <p className="p-about">
+                  <li>Read and manipulate Datasets</li>
+                  <li>DataFrames and Series objects</li>
+                  <li>Organize, filter, clean, aggregate, and analyze DataFrames</li>
+                  <li>Extract and manipulate date, time, and textual information from data</li>
+                  <li>Hierarchical Indexing</li>
+                  <li>Merge Datasets</li>
+                  <li>Create complex visualizations</li>
+                  </p>
+                  </div>
 
                   </div>
                 </div>
               </div>
               <div class="accordion-item">
-                <h5 class="accordion-header" id="panelsStayOpen-headingTwo">
-                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                    MySQL <div class="btn-container text-center"><a href="../../../MySQL.pdf" target="_blank" rel="noopener noreferrer"><Mortarboard className='card-icon2' data-bs-toggle="tooltip" data-bs-placement="top" title="See Diploma" /></a></div>
+                <h5 class="accordion-header" id="panelsStayOpen-headingThree">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                    MySQL <div class="btn-container text-center"><a href="../../../MySQL.pdf" target="_blank" rel="noopener noreferrer"><Mortarboard className='card-icon2' /></a></div>
                   </button>
                 </h5>
-                <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+                <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
                   <div class="accordion-body">
                     <strong>The Ultimate MySQL Bootcamp</strong> by Colt Steele in Udemy. A total of 37.5 hours. This course is a gentle but comprehensive introduction to MySQL:
                     <div className="col">
@@ -59,12 +88,12 @@ export class Certifications extends Component {
                 </div>
               </div>
               <div class="accordion-item">
-                <h5 class="accordion-header" id="panelsStayOpen-headingThree">
-                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                    Python Bootcamp <div class="btn-container text-center"><a href="../../../Python.pdf" target="_blank" rel="noopener noreferrer"><MortarboardFill className='card-icon2' data-bs-toggle="tooltip" data-bs-placement="top" title="See Diploma" /></a></div>
+                <h5 class="accordion-header" id="panelsStayOpen-headingFour">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFour" aria-expanded="false" aria-controls="panelsStayOpen-collapseFour">
+                    Python Bootcamp <div class="btn-container text-center"><a href="../../../Python.pdf" target="_blank" rel="noopener noreferrer"><MortarboardFill className='card-icon2' /></a></div>
                   </button>
                 </h5>
-                <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+                <div id="panelsStayOpen-collapseFour" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFour">
                   <div class="accordion-body">
                     <strong> Python essentials.</strong> by Colt Steele in Udemy. A total of 14.5 hours. This course exolores everything from variables to data structures to object oriented programming and modules:
                     <div className="col">
@@ -83,18 +112,17 @@ export class Certifications extends Component {
                 </div>
               </div>
               <div class="accordion-item">
-                <h5 class="accordion-header" id="panelsStayOpen-headingFour">
-                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFour" aria-expanded="false" aria-controls="panelsStayOpen-collapseFour">
-                  Business Management & Marketing<div class="btn-container text-center"><a href="../../../MKTN.pdf" target="_blank" rel="noopener noreferrer"><Mortarboard className='card-icon2' data-bs-toggle="tooltip" data-bs-placement="top" title="See Diploma" /></a></div>
+                <h5 class="accordion-header" id="panelsStayOpen-headingFive">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFive" aria-expanded="false" aria-controls="panelsStayOpen-collapseFive">
+                  Business Management & Marketing<div class="btn-container text-center"><a href="../../../MKTN.pdf" target="_blank" rel="noopener noreferrer"><Mortarboard className='card-icon2' /></a></div>
                   </button>
                 </h5>
-                <div id="panelsStayOpen-collapseFour" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFour">
+                <div id="panelsStayOpen-collapseFive" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFive">
                   <div class="accordion-body">
                     <strong>Businesses Management and Marketing</strong> associate's from the Córdoba National University. With a total of 800 hours.
                    </div>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
