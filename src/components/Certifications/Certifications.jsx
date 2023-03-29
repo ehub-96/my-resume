a50f43b30a1b7a1bd87433259fa26bdd11398db8
Erik Huß <ehub96.dev@gmail.com>
2023-03-29 13:28:55 -0600

    Back to blank v1

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -13,136 +13,86 @@ export class Certifications extends Component {
             <h1 className='s-title'>Certifications</h1>
             <div className="row">
 
-              <div class="accordion" id="accordionPanelsStayOpenExample">
-                <div class="accordion-item">
-                  <h2 class="accordion-header">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                      Accordion Item #1
-                    </button>
-                  </h2>
-                  <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse">
-                    <div class="accordion-body">
-                      <strong>BRUH </strong> BRUH BRUH BRUH <code>CHAD</code>GIGA BRUH
-                      <div class="accordion" id="accordionPanelsStayOpenExample">
-
-
-                        <div class="accordion accordion-flush">
-                          <h2 class="accordion-header">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#nestedpanelsStayOpen-collapseOne" aria-expanded="false" aria-controls="nestedpanelsStayOpen-collapseOne">
-                              Nested Accordion Item #1
-                            </button>
-                          </h2>
-                          <div id="nestedpanelsStayOpen-collapseOne" class="accordion-collapse collapse">
-                            <div class="accordion-body">
-                              <strong>BRUH </strong> BRUH BRUH BRUH <code>CHAD</code>GIGA BRUH
-                            </div>
-                          </div>
-                        </div>
-                        <div class="accordion accordion-flush">
-                          <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#nestedpanelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="nestedpanelsStayOpen-collapseTwo">
-                            Nested Accordion Item #2
-                            </button>
-                          </h2>
-                          <div id="nestedpanelsStayOpen-collapseTwo" class="accordion-collapse collapse">
-                            <div class="accordion-body">
-                              <strong>This is the second item's accordion body.</strong>BRUH BRUH<code>CHAD</code>GIGA BRUH
-                            </div>
-                          </div>
-                        </div>
-                        <div class="accordion accordion-flush">
-                          <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#nestedpanelsStayOpen-collapseThree" aria-expanded="false" aria-controls="nestedpanelsStayOpen-collapseThree">
-                            Nested Accordion Item #3
-                            </button>
-                          </h2>
-                          <div id="nestedpanelsStayOpen-collapseThree" class="accordion-collapse collapse">
-                            <div class="accordion-body">
-                              <strong>BRUH.</strong>BRUH BRUH<code>CHAD</code>GIGA BRUH
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+            <div class="accordion" id="accordionPanelsStayOpenExample">
+              <div class="accordion-item">
+                <h2 class="accordion-header">
+                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne">
+                    Accordion Item #1
+                  </button>
+                </h2>
+                <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse">
+                  <div class="accordion-body">
+                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                   </div>
                 </div>
+              </div>
 
 
-
-
-                <div class="accordion-item">
-                  <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                      Accordion Item #2
-                    </button>
-                  </h2>
-                  <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
-                    <div class="accordion-body">
-                      <strong>This is the second item's accordion body.</strong>BRUH BRUH<code>CHAD</code>GIGA BRUH
-                            <div class="accordion accordion-flush">
-                                <h2 class="accordion-header">
-                                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#nestedpanelsStayOpen1-collapseOne" aria-expanded="false" aria-controls="nestedpanelsStayOpen1-collapseOne">
-                                    Nested Accordion Item #1
-                                  </button>
-                                </h2>
-                                <div id="nestedpanelsStayOpen1-collapseOne" class="accordion-collapse collapse">
-                                  <div class="accordion-body">
-                                    <strong>BRUH </strong> BRUH BRUH BRUH <code>CHAD</code>GIGA BRUH
-                                  </div>
-                                </div>
-                              </div>
-                              <div class="accordion accordion-flush">
-                                <h2 class="accordion-header">
-                                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#nestedpanelsStayOpen1-collapseTwo" aria-expanded="false" aria-controls="nestedpanelsStayOpen1-collapseTwo">
-                                  Nested Accordion Item #2
-                                  </button>
-                                </h2>
-                                <div id="nestedpanelsStayOpen1-collapseTwo" class="accordion-collapse collapse">
-                                  <div class="accordion-body">
-                                    <strong>This is the second item's accordion body.</strong>BRUH BRUH<code>CHAD</code>GIGA BRUH
-                                  </div>
-                                </div>
-                              </div>
-                              <div class="accordion accordion-flush">
-                                <h2 class="accordion-header">
-                                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#nestedpanelsStayOpen1-collapseThree" aria-expanded="false" aria-controls="nestedpanelsStayOpen1-collapseThree">
-                                  Nested Accordion Item #3
-                                  </button>
-                                </h2>
-                                <div id="nestedpanelsStayOpen1-collapseThree" class="accordion-collapse collapse">
-                                  <div class="accordion-body">
-                                    <strong>BRUH.</strong>BRUH BRUH<code>CHAD</code>GIGA BRUH
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                    </div>
-
-
-
-
-                <div class="accordion-item">
-                  <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                      Accordion Item #3
-                    </button>
-                  </h2>
-                  <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
-                    <div class="accordion-body">
-                      <strong>BRUH.</strong>BRUH BRUH<code>CHAD</code>GIGA BRUH
-                    </div>
+              <div class="accordion-item">
+                <h2 class="accordion-header">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                    Accordion Item #2
+                  </button>
+                </h2>
+                <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
+                  <div class="accordion-body">
+                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                   </div>
+                </div>
+              </div>
+
+
+              <div class="accordion-item">
+                <h2 class="accordion-header">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                    Accordion Item #3
+                  </button>
+                </h2>
+                <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
+                  <div class="accordion-body">
+                    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
 
 
 
 
-            
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+
+
         
         );
     }
