c7a2e257a916409d84ad4deb297bcfe0d0d6798e
Erik Huß <ehub96.dev@gmail.com>
2023-03-29 12:56:35 -0600

    (?)

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -122,7 +122,7 @@ export class Certifications extends Component {
 
 
 
-            <div class="accordion-item">
+             <div class="accordion-item">
               <h2 class="accordion-header">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
                   Accordion Item #3
@@ -134,7 +134,9 @@ export class Certifications extends Component {
                 </div>
               </div>
             </div>
-          </div>
+
+
+           </div>
 
             
         
