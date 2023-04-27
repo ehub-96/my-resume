bed8485ba38d6c8e4c179260e38f5dcf677ffff2
Erik Huß <ehub96.dev@gmail.com>
2023-04-26 22:29:36 -0600

    Accordion V3 DA finished

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -62,37 +62,67 @@ export class Certifications extends Component {
                 </h2>
                 <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
                   <div class="accordion-body">
-                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-
+                    
                       <div class="accordion accordion-flush" id="accordionFlushExample1">
                           <div class="accordion-item">
                             <h2 class="accordion-header">
                               <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne1" aria-expanded="false" aria-controls="flush-collapseOne1">
-                                Nested Accordion Item #1
+                                Google Data Analytics Certificate<div class="btn-container text-center"><a href="" target="_blank" rel="noopener noreferrer"><MortarboardFill className='card-icon2' data-bs-toggle="tooltip" data-bs-placement="top" title="Waiting for Diploma" /></a></div>
                               </button>
                             </h2>
                             <div id="flush-collapseOne1" class="accordion-collapse collapse">
-                              <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                              <div class="accordion-body">Data Analytics <strong>By Google</strong>, a comprehensive 6 months program of 8 courses, with deep understanding of industry practices and key role skills. </div>
+                              <div className="col">
+                                      <p className="p-about">
+                                      <li>The Role of Data</li>
+                                      <li>Data-Driven Decisions</li>
+                                      <li>Data Exploratio & Data Cleaning</li>
+                                      <li>Data Visualization</li>
+                                      <li>Data & R</li>                                      
+                                      </p>
+                                      </div>
                             </div>
                           </div>
                           <div class="accordion-item">
                             <h2 class="accordion-header">
                               <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo1" aria-expanded="false" aria-controls="flush-collapseTwo1">
-                              Nested Accordion Item #2
+                              Data Analysis & Visualization <div class="btn-container text-center"><a href="../../../DA.pdf" target="_blank" rel="noopener noreferrer"><Mortarboard className='card-icon2' data-bs-toggle="tooltip" data-bs-placement="top" title="See Diploma" /></a></div>
                               </button>
                             </h2>
                             <div id="flush-collapseTwo1" class="accordion-collapse collapse">
-                              <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+                              <div class="accordion-body"><strong>2022 Python Data Analysis & Visualization Masterclass</strong> by Colt Steele in Udemy. This course provides an in-depth understanding of the Pandas, Matplotlib, and Seaborn libraries, as well as other essential tools for data analysis using a wide range of real-world datasets to ensure hands-on experience, covering:</div>
+                              <div className="col">
+                                <p className="p-about">
+                                <li>Read and manipulate Datasets</li>
+                                <li>DataFrames and Series objects</li>
+                                <li>Organize, filter, clean, aggregate, and analyze DataFrames</li>
+                                <li>Extract and manipulate date, time, and textual information from data</li>
+                                <li>Hierarchical Indexing</li>
+                                <li>Merge Datasets</li>
+                                <li>Create complex visualizations</li>
+                                </p>
+                                </div>
                             </div>
                           </div>
                           <div class="accordion-item">
                             <h2 class="accordion-header">
                               <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree1" aria-expanded="false" aria-controls="flush-collapseThree1">
-                              Nested Accordion Item #3
+                              MySQL <div class="btn-container text-center"><a href="../../../MySQL.pdf" target="_blank" rel="noopener noreferrer"><MortarboardFill className='card-icon2' data-bs-toggle="tooltip" data-bs-placement="top" title="See Diploma"/></a></div>
                               </button>
                             </h2>
                             <div id="flush-collapseThree1" class="accordion-collapse collapse">
-                              <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                              <div class="accordion-body"><strong>The Ultimate MySQL Bootcamp</strong> by Colt Steele in Udemy. This course is a comprehensive introduction to databases and SQL syntax and complex queries.</div>
+                              <div className="col">
+                                <p className="p-about">
+                                <li>SQL syntax</li>
+                                <li>Reports</li>
+                                <li>Aggregate Functions</li>
+                                <li>SQL joins</li>
+                                <li>MySQL 8.x Window Functions</li>
+                                <li>MySQL database views and virtual tables</li>
+                                <li>Design and implement complex database schemas</li>
+                                </p>
+                                </div>
                             </div>
                           </div>
                         </div>
