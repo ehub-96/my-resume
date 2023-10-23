30925d28231466d39aa176e7322de236e1ed1899
Erik Huß <ehub96.dev@gmail.com>
2023-10-23 15:05:57 -0300

    Title

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <div className="col-sm-6">
             <div className="main-title">
                 <img src={Ehub}/>
-                <h2 className='title'>Erik Hub - Data Consultant</h2>
+                <h2 className='title'>Erik Hub - Data Engineer</h2>
                 <div class="btn-container text-center">
 
                   <div class="row">
