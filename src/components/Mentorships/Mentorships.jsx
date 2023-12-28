e76472ab34070792b98f2ed61b669fb71ace4234
Erik Huß <ehub96.dev@gmail.com>
2023-12-28 14:37:52 +0100

    Nova

--- src/components/Mentorships/Mentorships.jsx
+++ src/components/Mentorships/Mentorships.jsx
@@ -7,76 +7,76 @@ import erwin from"../../../public/erwin.jpg";
 
 
 
-export class Mentorships extends Component {
-  render() {
-    return (
-        <div className='Mentorships'>
-            <h1 className='s-title'>Mentorships</h1>
-            <div className="row">
-            <div className="row">
-              <p>This area is dedicated to my friends who convinced me to learn coding and pushed me to get a foot in the Tech Industry.</p>
-            </div>
+// export class Mentorships extends Component {
+//   render() {
+//     return (
+//         <div className='Mentorships'>
+//             <h1 className='s-title'>Mentorships</h1>
+//             <div className="row">
+//             <div className="row">
+//               <p>This area is dedicated to my friends who convinced me to learn coding and pushed me to get a foot in the Tech Industry.</p>
+//             </div>
 
             
           
-            <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
-              <div class="col">
-                <div class="card mb-3">
-                  <img src="https://i.pinimg.com/originals/a1/9a/11/a19a115e65e5146254f8646fb08b2db5.jpg" class="card-img-top2" alt=""/>
-                  <div class="card-body">
-                    <h5 class="card-title">Gastón Gigena</h5>
-                    <p class="card-text">QA engineer at Avature, Computer technician and Python freelance developer.</p>
-                  </div>
-                  <div class="card-footer">
-                  <div className="col-sm text-center">
+//             <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+//               <div class="col">
+//                 <div class="card mb-3">
+//                   <img src="https://i.pinimg.com/originals/a1/9a/11/a19a115e65e5146254f8646fb08b2db5.jpg" class="card-img-top2" alt=""/>
+//                   <div class="card-body">
+//                     <h5 class="card-title">Gastón Gigena</h5>
+//                     <p class="card-text">QA engineer at Avature, Computer technician and Python freelance developer.</p>
+//                   </div>
+//                   <div class="card-footer">
+//                   <div className="col-sm text-center">
                     
-                    <a href="https://www.linkedin.com/in/gaston-gigena/" target="_blank" rel="noopener noreferrer"><Linkedin className='card-icon2' /></a>
+//                     <a href="https://www.linkedin.com/in/gaston-gigena/" target="_blank" rel="noopener noreferrer"><Linkedin className='card-icon2' /></a>
                     
-                  </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card mb-3">
-                  <img src="https://i.pinimg.com/originals/a1/d1/80/a1d1803413718f67662dbb9658bd86d8.jpg" class="card-img-top2" alt="..."/>
-                  <div class="card-body">
-                    <h5 class="card-title">Erwin Arndt</h5>
-                    <p class="card-text">Office IT Administrator - Operations Technician at Westpay AB.</p>
-                  </div>
-                  <div class="card-footer">
-                  <div className="col-sm text-center">
+//                   </div>
+//                   </div>
+//                 </div>
+//               </div>
+//               <div class="col">
+//                 <div class="card mb-3">
+//                   <img src="https://i.pinimg.com/originals/a1/d1/80/a1d1803413718f67662dbb9658bd86d8.jpg" class="card-img-top2" alt="..."/>
+//                   <div class="card-body">
+//                     <h5 class="card-title">Erwin Arndt</h5>
+//                     <p class="card-text">Office IT Administrator - Operations Technician at Westpay AB.</p>
+//                   </div>
+//                   <div class="card-footer">
+//                   <div className="col-sm text-center">
                     
-                    <a href="https://www.linkedin.com/in/erwinjohannarndt/" target="_blank" rel="noopener noreferrer"><Linkedin className='card-icon2' /></a>
+//                     <a href="https://www.linkedin.com/in/erwinjohannarndt/" target="_blank" rel="noopener noreferrer"><Linkedin className='card-icon2' /></a>
                     
-                  </div>
-                  </div>
-                </div>
-              </div>
-              {/* <div class="col">
-                <div class="card border-danger mb-3">
-                  <img src="https://writedirection.com/website/wp-content/uploads/2016/09/blank-profile-picture-973460_960_720.png" class="card-img-top2" alt="..."/>
-                  <div class="card-body">
-                    <h5 class="card-title">Lucas Maldonado</h5>
-                    <p class="card-text">-
-                    -</p>
-                  </div>
-                  <div class="card-footer">
-                  <div className="col-sm text-center">
-                    <a href="https://github.com/ehub-96" target="_blank" rel="noopener noreferrer"><Github className='card-icon2' /></a>
-                    <a href="_blank" target="_blank" rel="noopener noreferrer"><Linkedin className='card-icon2' /></a>
-                    <a href="https://twitter.com/ehub96?t=hQ_1F1a5bBjxX6k1RJCqQg&s=08" target="_blank" rel="noopener noreferrer"><Twitter className='card-icon2' /></a>
-                  </div>
-                  </div>
-                </div>
-              </div> */}
-            </div>
+//                   </div>
+//                   </div>
+//                 </div>
+//               </div>
+//               <div class="col">
+//                 <div class="card border-danger mb-3">
+//                   <img src="https://writedirection.com/website/wp-content/uploads/2016/09/blank-profile-picture-973460_960_720.png" class="card-img-top2" alt="..."/>
+//                   <div class="card-body">
+//                     <h5 class="card-title">Lucas Maldonado</h5>
+//                     <p class="card-text">-
+//                     -</p>
+//                   </div>
+//                   <div class="card-footer">
+//                   <div className="col-sm text-center">
+//                     <a href="https://github.com/ehub-96" target="_blank" rel="noopener noreferrer"><Github className='card-icon2' /></a>
+//                     <a href="_blank" target="_blank" rel="noopener noreferrer"><Linkedin className='card-icon2' /></a>
+//                     <a href="https://twitter.com/ehub96?t=hQ_1F1a5bBjxX6k1RJCqQg&s=08" target="_blank" rel="noopener noreferrer"><Twitter className='card-icon2' /></a>
+//                   </div>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
           
             
 
-            </div>
-        </div>
-      );
-  }
-}
+//             </div>
+//         </div>
+//       );
+//   }
+// }
 
-export default Mentorships;
+// export default Mentorships;
